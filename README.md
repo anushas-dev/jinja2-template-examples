05ddc9433b7a274d07d7dab754f00b95ce3aa19e
Anusha S <72661377+anushas-dev@users.noreply.github.com>
2022-06-18 13:53:37 +0530

    added examples

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # jinja2-template-examples
+
+The example coverer te following filters for the string manipulation for Jinja Templating:
+- Title
+- Upper
+- Lower
+- Unique
+- Max
+- For Loop
+
+If you getting started with Jinja2 and would like to learn Basics:
+- Free Course on [KodeKloud](https://kodekloud.com/courses/jinja2-templating/)
---
+++ format-para.py
@@ -0,0 +1,30 @@
+from jinja2 import Template
+
+
+numbers = [1,2,3,4,5,6,7,8]
+template = """Hello {{name | title}},
+Hope you are doing well!
+Please confirm the following order:
+{{quantity | unique | max}} {{type | replace ("books","NOTEBOOKS") | lower}}, {% for key, value in collection.items() %} 
+{{ value }} {{key}} {% endfor %}
+We'll initiate the delivery after your confirmation. 
+
+{{note | upper}}
+
+Yours sincerely,
+{{dept}} """
+
+data = {
+    "name": "john doe",
+    "note": "Offer: Until end of this month, we are offering 50% off on all our products",
+    "type": "books",
+    "dept": "Girija BookStore",
+    "quantity": [2,4,5,2,2,8,9,9,10,2,2,6],
+    "collection" : {"Pencils":"2","Pens":"3", "Stencils":"5", "Erasers":"5" }
+}
+
+j2_template = Template(template)
+
+print(j2_template.render(data))
+# print(Template(order_info.txt).render(data))
+
